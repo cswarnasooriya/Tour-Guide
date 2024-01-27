3a0bda2fde98539fdcb1a85742c35ba25f02b20d
Chanuka Sandaruwan Warnasooriya <warnasooriyacs1000@gmail.com>
2024-01-27 13:03:32 +0530

    Navbar updated

--- tour_guide/app/layout.tsx
+++ tour_guide/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
+
 
 
 export const metadata: Metadata = {
@@ -16,8 +19,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-
-        {children}
+        <Navbar/>
+        
+        <main className="relative overflow-hidden">
+          {children}
+        </main>
+        
+        <Footer/>
         
       </body>
     </html>
