bc3b97056f0f5eab8d4303efcaa4755c355ed39e
Chanuka Sandaruwan Warnasooriya <warnasooriyacs1000@gmail.com>
2024-02-07 10:30:44 +0530

    Feature section Created

--- tour_guide/app/page.tsx
+++ tour_guide/app/page.tsx
@@ -1,7 +1,6 @@
 import Hero from "@/components/Hero";
 import Camp from "@/components/Camp";
 import Guide from "@/components/Guide";
-import GetApp from '@/components/GetApp';
 import Features from "@/components/Features";
 
 
@@ -12,6 +11,7 @@ export default function Home() {
       <Hero/>
       <Camp/>
       <Guide/>
+      <Features/>
       
       
 
--- tour_guide/components/Features.tsx
+++ tour_guide/components/Features.tsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import Image from 'next/image'
 
 const Features = () => {
   return (
-    <div>
+    <section className="border-2 border-red-500 flex-col flexCenter overflow-hidden bg-feature-bg bg-center bg-no-repeat py-24">
+      <div className="max-container padding-container relative w-full justify-end">
+        <div>
+          <Image
+            src="/phone.png"
+            alt="phone"
+            width={400}
+            height={1000}
+            className="feature-phone"
+          
+          
+          />
+        </div>
+      </div>
       
-    </div>
+    </section>
   )
 }
 
